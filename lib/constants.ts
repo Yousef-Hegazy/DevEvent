67e5d52840858ea5cc808592e06b528ab7ecd05d
Yousef-Hegazy <yousef.ashraf@syadtech.com>
2025-11-13 16:41:57 +0300

    Initial Commit with home page

---
+++ lib/constants.ts
@@ -0,0 +1,51 @@
+export const events = [
+  {
+    title: 'React Summit 2025',
+    image: '/images/event1.png',
+    slug: 'react-summit-2025',
+    location: 'Amsterdam, Netherlands',
+    date: 'November 15-16, 2025',
+    time: '9:00 AM - 6:00 PM',
+  },
+  {
+    title: 'AWS re:Invent 2025',
+    image: '/images/event2.png',
+    slug: 'aws-reinvent-2025',
+    location: 'Las Vegas, NV',
+    date: 'December 2-6, 2025',
+    time: '8:00 AM - 8:00 PM',
+  },
+  {
+    title: 'CES 2026',
+    image: '/images/event3.png',
+    slug: 'ces-2026',
+    location: 'Las Vegas, NV',
+    date: 'January 7-10, 2026',
+    time: '9:00 AM - 6:00 PM',
+  },
+  {
+    title: 'DevFest 2026',
+    image: '/images/event4.png',
+    slug: 'devfest-2026',
+    location: 'Mountain View, CA',
+    date: 'February 20, 2026',
+    time: '10:00 AM - 7:00 PM',
+  },
+  {
+    title: 'GitHub Universe 2026',
+    image: '/images/event5.png',
+    slug: 'github-universe-2026',
+    location: 'San Francisco, CA',
+    date: 'March 15-16, 2026',
+    time: '9:00 AM - 5:00 PM',
+  },
+  {
+    title: 'JSConf 2026',
+    image: '/images/event6.png',
+    slug: 'jsconf-2026',
+    location: 'Berlin, Germany',
+    date: 'June 12-14, 2026',
+    time: '9:00 AM - 6:00 PM',
+  },
+]
+
